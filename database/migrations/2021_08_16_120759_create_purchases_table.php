909d4ef90cdfe26f2e0200bbb6139705a40dbe91
Maiyoj <valentinemaiyo1995@gmail.com>
2021-08-24 09:27:46 +0300

    ftables changes

--- database/migrations/2021_08_16_120759_create_purchases_table.php
+++ database/migrations/2021_08_16_120759_create_purchases_table.php
@@ -19,7 +19,7 @@ class CreatePurchasesTable extends Migration
             $table->integer('vendor_id');
             $table->string('PO_number');
             $table->integer('quantity');
-            $table->longInt('price');
+            $table->float('price');
             $table->timestamps();
         });
     }
