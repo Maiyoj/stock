18624bef34168456bad794106dbe2e7049683050
Maiyoj <valentinemaiyo1995@gmail.com>
2022-01-04 09:04:10 +0300

    changeinapprovals

--- resources/views/approve/index.blade.php
+++ resources/views/approve/index.blade.php
@@ -194,44 +194,6 @@
                           </div>
                         </div>
                       </div>
-                      <script>
-                        $(function(e)
-                        {
-                           $("#chkCheckAll").click(function(){
-                               $(".checkBoxClass").prop('checked', $(this).prop('checked'));
-                           });
-                       
-                       $("#deleteAllSelectedRecord").click(function(e){
-                       
-                        e.preventDefault();
-                        var allids = [];  
-                          $("input:checkbox[name=ids]:checked").each(function(){
-                       
-                           allids.push($(this).val());
-                          });
-                    
-                        
-                          $.ajax({
-                       
-                           url:"{{route('approve.delete')}}",
-                           type:"DELETE",
-                           data:{
-                               _token:$("input[name=_token]").val(),
-                            ids:allids
-                           },
-                           success:function(response){
-                               $.each(allids, function(key,val){
-                                $("#sid"+val).remove();  
-                                alert(response.success);
-                                location.reload(true); 
-                               })
-                           }
-                          });
-                       
-                       })
-                       
-                     });
-                    
-                    </script>
+                      
                 </main>
 @endsection  
