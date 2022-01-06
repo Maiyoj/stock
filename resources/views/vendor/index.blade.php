c3ac3e601556800223a5e716ad45838fce2b2f55
Maiyoj <valentinemaiyo1995@gmail.com>
2022-01-06 11:39:53 +0300

    uiupdate

--- resources/views/vendor/index.blade.php
+++ resources/views/vendor/index.blade.php
@@ -130,14 +130,12 @@
                                      </td>
                                             </tr>                                                           
                                         @empty
-                                        
-
                                         @endforelse
-                                  
-                                        
-                                       
                                     </tbody>
                                 </table>
+                                <div class="d-flex flex-row-reverse bd-highlight">
+                                    <div class="p-2 bd-highlight"> <a class="btn btn-primary" href="/admin">Go Back</a></div>
+                                  </div>
                             </div>
                         </div>
                     </div>
